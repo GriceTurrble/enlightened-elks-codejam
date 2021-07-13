e739c361e7ea33fe806f57a252d7163f63a61860
Evan <eokeefe3@wisc.edu>
2021-07-13 11:52:51 -0500

    Fixed linting issue

--- experiments/snake.py
+++ experiments/snake.py
@@ -1,5 +1,6 @@
 """
 This module will display a snake game within the command line interface.
+The user input will appear on the left and the game on the right.
 """
 
 from prompt_toolkit import Application
